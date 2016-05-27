711c9a81caf03780d4dfcccf8ba26beee32a849b
秦晋 <qjdev@qq.com>
2016-05-27 17:57:40 +0800

    TinyMapperTest完成

---
+++ TinyMapperTest/Fakes/FakeClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyMapperTest.Fakes
+{
+    public class FakeClass
+    {
+        public Guid Id { get; set; }
+
+        public List<InnerClass> InnerClasses { get; set; } 
+
+        public class InnerClass
+        {
+            public Guid InnerClassGuid { get; set; }
+        }
+    }
+}
